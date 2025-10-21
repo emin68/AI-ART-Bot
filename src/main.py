bbcf83433071f5f15a959a5b624fb7ffa4f3ece3
pc fixe emin <pc fixe emin>
2025-10-21 15:27:02 +0200

    modif scraped websites

--- src/main.py
+++ src/main.py
@@ -17,16 +17,9 @@ SITES = [
     ("https://news.artnet.com/market", "Artnet market"),
     ("https://news.artnet.com/multimedia", "Artnet multimedia"),
     ("https://techcrunch.com/category/artificial-intelligence/", "TechCrunch"),
-    ("https://fr.artprice.com/artprice-news", "Artprice"),
-    ("https://fr.artprice.com/artmarketinsight", "ArtPrice ArtMarketInsight"),
     ("https://fr.cointelegraph.com/tags/nft", "Cointelegraph NFT"),
     ("https://fr.cointelegraph.com/tags/ai", "Cointelegraph AI"),
-    ("https://news.artnet.com/multimedia", "Dezeen Technology"),
-    ("https://www.fastcompany.com/technology", "Fastcompany Tech"),
-    ("https://www.engadget.com/ai/", "Engadget AI"),
-    ("https://www.engadget.com/science/robotics/", "Engadget robotics"),
 ]
-
 
 # --- Gestion du cache ---
 CACHE_FILE = Path("data/cache/seen_urls.txt")
